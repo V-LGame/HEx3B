9e9a8a269328a2e35f2f07d35eb16a3d86289c16
Victor Kushnir <kushnir.victor@gmail.com>
2021-11-03 18:32:49 +0200

    .

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,14 +8,17 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 moveDirection;
     public SpriteRenderer mySpriteRenderer;
+    //* Updates every frame.
     void Update()
     {
         ProcessInputs();
     }
+    //* Updates every fixed amount of frames.
     void FixedUpdate()
     {
         Move();
     }
+    //* Calculating the changes for the position and swaps the image of the players image by X axis respectivly.
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
@@ -28,9 +31,11 @@ public class PlayerMovement : MonoBehaviour
         {
             mySpriteRenderer.flipX = false;
         }
+        //* (.normalized) was added so the movement in 2 axis simultaneously wont double the speed . 
         moveDirection = new Vector2(moveX, moveY).normalized;
         
     }
+    //*Calculates the velocity of the the player using its position and Unity added speed.
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
